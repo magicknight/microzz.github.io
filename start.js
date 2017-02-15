e3bf9a0d7f730fd0a9603246188f45a286fdd37c
microzz <903036377@qq.com>
2017-02-15 15:09:00 +0800

    Site updated: 2017-02-15 15:09:00

--- start.js
+++
@@ -1,7 +0,0 @@
-var process = require('child_process');
-
-process.exec('hexo server -p 80', function (error, stdout, stderr) {
-    if (error !== null) {
-        console.log('exec error: ' + error);
-    }
-});
